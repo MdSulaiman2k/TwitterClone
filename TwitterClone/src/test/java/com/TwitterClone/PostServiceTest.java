05718bffd337070a06a13d4fae85e8f115c63661
sulaiman <mdsulaiman132k@gmail.com>
2021-07-27 10:51:03 +0530

    Add PostServiceTest

--- TwitterClone/src/test/java/com/TwitterClone/PostServiceTest.java
+++ TwitterClone/src/test/java/com/TwitterClone/PostServiceTest.java
@@ -1,5 +1,44 @@
 package com.TwitterClone;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import javax.xml.bind.ValidationException;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.TwitterClone.Dto.PostReqDto;
+import com.TwitterClone.Model.Post;
+import com.TwitterClone.Service.PostService;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 public class PostServiceTest {
+	
+	@Autowired
+	PostService postService ;
+	
+	@Test
+	@Order(1)
+	public void savePostTest() throws ValidationException {
+		PostReqDto postReq = new PostReqDto("Texting post" , 1);
+		Post post = postService.createPost(postReq) ;
+		assertNotNull(postService.findPost(post.getId()));
+	}
+	
+	@Test
+	@Order(2)
+	public void readPostTest() throws ValidationException {
+		List<Post> postList = postService.findAllPost() ;
+		assertThat(postList).size().isGreaterThan(0);	
+	}
+	
 
 }
